3364336384bfcfdb509ce97ccbe0be3c5c9c2222
Janyk Steenbeek <janyk@brisp.nl>
2020-12-20 20:02:20 +0100

    wip

--- README.md
+++ README.md
@@ -1,57 +1,35 @@
+A Telegram bot to bridge UniFi Video e-mail alerts with Telegram messages in a single group.
+When motion is detected, you will receive a message in the configured group with the snapshot and snapshot timestamp. It will also send the subject of non-motion alert emails, e.g: camera disconnect/reconnect
 
-# What's this?
-
-A telegram bot to bridge unifi-video email alerts with telegram notifications/messages in a single group.
-It will also send the subject of non-motion alert emails, e.g: camera disconnect/reconnect
-
-The bot supports 3 commands:
-
- * /start: tell the bot to send messages to the group where the command is issued, ATM only a single group is supported.
- * /st, /status: show status (active/inactive)
- * /ac, /activate: enable notifications
- * /de, /deactivate: disable notifications, the will receive the email alerts but not forward them to telegram
-
- in/active state is persistent, and will be maintained across restarts
-
-## Bot setup
-
- 1) create a bot via [BotFather](https://core.telegram.org/bots#6-botfather) and grab the token 
- 2) create a group and add your newly created bot to it
- 3) create a config based on the [config.yaml.example](config.yaml.example) using the Token and adjust for your system/users
  
+## Setting up as Docker container 
 
-### virtualenv 
-
-1) create a virtualenv:
-
-    $ python3 -m venv venv
-
-2) install deps 
-
-    $ source venv/bin/activate 
-    $ pip install -r requirements.txt 
-
-3) python ./uv_alert_bot.py config.yaml 
-
-### fades 
-
-1) install [fades](https://pypi.python.org/pypi/fades )
-
-2) run it
-
-    fades ./uv_alert_bot.py config.yaml 
-
-### Docker 
-
-1) docker build . 
+1) Build the container by running `docker build .` 
 
 2) docker run -v /path/to/config_dir/:/config --net=host
 
-## Unifi video setup
+## Setting up the bot
 
-Go to https://your-unifi-video-host:7443/settings/main
-  1) enable email alerts 
-  2) In the email settings:
-     smtp server: IP/hostname where the bot is running
-     port: 8025 (or the port you specified in the config.yaml)
-  3) save changes 
+ 1) Create a bot via [BotFather](https://core.telegram.org/bots#6-botfather) and grab the token 
+ 2) Create a group and add your newly created bot to it
+ 3) Create a config based on the [config.yaml.example](config/config.yaml.example) using the token and adjust for your system/users
+ 4) Run `/start` and `/enable` in the group  
+
+## Setting up UniFi Video
+
+Visit your UniFi Video settings and follow the following steps:
+  1) Enable e-mail alerts in your UniFi Video environment
+  2) Use the following settings:
+     - SMTP server: `127.0.0.1` (or the port you specified in the config.yaml) 
+     - Port: `8025` (or the port you specified in the config.yaml)
+  3) Save changes
+  
+## Commands
+The bot supports 3 commands:
+
+ * /start: tell the bot to send messages to the group where the command is issued, ATM only a single group is supported.
+ * /status: show status (active/inactive)
+ * /e, /enable: enable notifications
+ * /de, /disable: disable notifications, the will receive the email alerts but not forward them to telegram
+
+ in/active state is persistent, and will be maintained across restarts
--- uv_alert_bot.py
+++ uv_alert_bot.py
@@ -3,19 +3,16 @@ import base64
 import time
 import logging
 import shelve
-import tempfile
 
 from io import BytesIO
 
-import yaml # fades.pypi pyyaml == 3.12
-import telepot # fades.pypi == 12.1
+import yaml  # fades.pypi pyyaml == 3.12
+import telepot  # fades.pypi == 12.1
 import telepot.aio
 from telepot.aio.loop import MessageLoop
 
-from aiosmtpd.controller import Controller # fades.pypi == 1.1
+from aiosmtpd.controller import Controller  # fades.pypi == 1.1
 from aiosmtpd.handlers import Message
-
-import ftp
 
 
 class UnifiAlertMessage(Message):
@@ -27,17 +24,18 @@ class UnifiAlertMessage(Message):
     def handle_message(self, message):
         logging.info("Received email, bot state: %s - subject: %s",
                      self.bot.active, message.get('subject', 'no-subject'))
-        if self.bot.active:
-            parts = [part for part in message.walk()
-                     if part.get_content_type() == 'image/jpeg']
-            if not parts:
-                self.bot.sendMessage(message['subject'])
-            for part in parts:
-                image = BytesIO(base64.decodebytes(
-                    part.get_payload().encode('utf-8')))
-                self.bot.sendPhoto(image, topic="email")
-        else:
+        if not self.bot.active:
             logging.info("Bot is not active, ignoring email")
+            return
+
+        parts = [part for part in message.walk()
+                 if part.get_content_type() == 'image/jpeg']
+        if not parts:
+            self.bot.sendMessage(message['subject'])
+        for part in parts:
+            image = BytesIO(base64.decodebytes(
+                part.get_payload().encode('utf-8')))
+            self.bot.sendPhoto(image)
 
     async def smtpd_main(self, hostname, port):
         cont = Controller(self, hostname=hostname, port=port)
@@ -45,7 +43,6 @@ class UnifiAlertMessage(Message):
 
 
 class AlertBot:
-
     message_limit = 1
     no_call = 0
 
@@ -56,28 +53,13 @@ class AlertBot:
         self.loop = loop
         self.bot = bot
         self.active = self.state.get('active')
-        self.topics = {"ftp": {}, "email": {}}
 
     def sendMessage(self, message):
         if self.state.get('group') and self.active:
             self.loop.create_task(self.bot.sendMessage(
                 self.state.get('group')['id'], message))
 
-    def sendPhoto(self, image, topic):
-        if topic == 'ftp':
-            now = time.time()
-            last_call =  self.topics[topic].get('last_call', self.no_call)
-            call_delta = now - last_call
-            logging.info("Got image from: %s - last_call: %s - delta: %s",
-                         topic, last_call, call_delta)
-            if self.state.get('group') and self.active:
-                if last_call == self.no_call or \
-                        call_delta > self.message_limit:
-                    self.topics[topic]['last_call'] = now
-                else:
-                    logging.info("Throttling message from topic: %s - "
-                                 "delta: %s", topic, call_delta)
-                    return
+    def sendPhoto(self, image):
         self.loop.create_task(self.bot.sendPhoto(
             self.state.get('group')['id'], image))
 
@@ -85,16 +67,13 @@ class AlertBot:
         content_type, chat_type, chat_id = telepot.glance(msg)
         logging.info("Message received: content: %s, type: %s, chat_id: %s, "
                      "msg: %s", content_type, chat_type, chat_id, msg)
-        user = msg['from']['id'] # was username, some users don't have one
+        user = msg['from']['id']  # was username, some users don't have one
         if user not in self.valid_users:
             logging.warning("Message from UNKNOWN user: %s, content: %s, type:"
                             " %s, chat_id: %s, msg: %s", user, content_type,
                             chat_type, chat_id, msg)
-            await self.bot.sendMessage(chat_id, "Sorry, I can't do that. "
-                                       "Please contact the bot admin")
             return
         if 'text' not in msg:
-            # ignore it
             logging.debug("Ignoring message: %s", msg)
             return
         if chat_type == 'group' and msg['text'] == '/start':
@@ -102,59 +81,48 @@ class AlertBot:
             if group:
                 logging.warning("Already bound to a group: %s", group['title'])
                 if group['id'] == chat_id:
-
-                    await self.bot.sendMessage(chat_id,
-                                               "Already using this group")
-                else:
-                    await self.bot.sendMessage(
-                        chat_id, "Already using group: {}".format(group))
+                    await self.bot.sendMessage(chat_id, "Already using this group")
             else:
                 self.state['group'] = msg['chat']
                 return
-        if msg['text'] in ['/ac', '/activate']:
+        if msg['text'] in ['/e', '/enable']:
             self.active = True
             self.state['active'] = self.active
-        elif msg['text'] in ['/de', '/deactivate']:
+        elif msg['text'] in ['/d', '/disable']:
             self.active = False
             self.state['active'] = self.active
-        elif msg['text'] in ['/st', '/status']:
-            status = "Desactivada"
-            if self.active:
-                status = "Activada"
+        elif msg['text'] in ['/status']:
+            status = "The bot is active and sharing motion events" \
+                if self.active else "The bot is currently not sharing motion events"
             await self.bot.sendMessage(chat_id, status)
-
-    def ftp_event(self, topic, filepath):
-        if topic == 'ftpd_file_received' and self.active:
-            with open(filepath, 'rb') as fd:
-                self.sendPhoto(fd.read(), topic="ftp")
 
 
 def main(config, state):
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
     loop = asyncio.get_event_loop()
     bot = telepot.aio.Bot(config['token'])
     alert_bot = AlertBot(bot, loop, config['valid_users'], state)
     email_handler = UnifiAlertMessage(alert_bot)
     smtp_listen = config.get('smtp_listen', '0.0.0.0')
     smtp_port = config.get('smtp_port', 8025)
+
     # create smtpd task
     loop.create_task(email_handler.smtpd_main(smtp_listen, smtp_port))
-    # create the ftpd task
-    ftp_user = config['ftp']['user']
-    ftp_password = config['ftp']['password']
-    ftp_port = config['ftp']['port']
-    ftp_dir = tempfile.mkdtemp()
-    loop.run_in_executor(None, ftp.run_ftpd, '0.0.0.0', ftp_port, alert_bot,
-                         ftp_user, ftp_password, ftp_dir)
     # create bot task
+
     loop.create_task(MessageLoop(bot, alert_bot.handle).run_forever())
-    logging.info("Starting Bot and smtpd")
+    logging.info("Bot and SMTP server starting...")
     loop.run_forever()
 
 
 if __name__ == '__main__':
     # load config
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('config')
     options = parser.parse_args()
